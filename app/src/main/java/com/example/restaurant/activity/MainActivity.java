9d91686ba932ed3376f80427bae00115384afa39
Cédric Carro <cedric.carro@free.fr>
2024-01-25 12:11:54 +0100

    wip (bd request) setup parse api and services

--- app/src/main/java/com/example/restaurant/activity/MainActivity.java
+++ app/src/main/java/com/example/restaurant/activity/MainActivity.java
@@ -1,16 +1,25 @@
 package com.example.restaurant.activity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.restaurant.R;
+import com.example.restaurant.data.Restaurant;
+import com.example.restaurant.services.RestaurantServices;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final RestaurantServices restaurantServices = new RestaurantServices();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ArrayList<Restaurant> restaurants = restaurantServices.parseRestaurants();
+        Log.d("MainActivity","1 - " + restaurants.size());
+
     }
 }
