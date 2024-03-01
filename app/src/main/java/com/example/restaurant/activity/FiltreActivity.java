303d8fdcdafb55a4642b12848faa8ff4058800c3
BBtocchi <bertocchileadu32@gmail.com>
2024-03-01 13:24:01 +0100

    temp2

--- app/src/main/java/com/example/restaurant/activity/FiltreActivity.java
+++ app/src/main/java/com/example/restaurant/activity/FiltreActivity.java
@@ -2,10 +2,15 @@ package com.example.restaurant.activity;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.restaurant.R;
+
+import java.io.IOException;
 
 public class FiltreActivity extends AppCompatActivity {
     @Override
@@ -13,13 +18,22 @@ public class FiltreActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filtre);
 
-        byte[] bytes = getIntent().getByteArrayExtra("image");
+        String path = getIntent().getStringExtra("imagePath");
+        Uri uri = Uri.parse(path);
+        Log.d("uri",uri.getPath());
 
 
-        if (bytes != null) {
-            Bitmap imageBitmap = BitmapFactory.decodeByteArray(bytes,0,bytes.length);
+        if (path != null) {
             ImageView imageView = findViewById(R.id.PhotoPrise);
-            imageView.setImageBitmap(imageBitmap);
+            // Charger l'image à partir du chemin absolu et l'afficher dans l'ImageView
+            Bitmap bitmap = null;
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            Bitmap bitmap2 = bitmap.copy(bitmap.getConfig(), true);
+            imageView.setImageBitmap(bitmap2);
         }
 
     }
