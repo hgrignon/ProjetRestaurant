9066aa8ed70bc107114c8bf235d242ee21688b70
hgrignon <heloise.grignon@gmail.com>
2024-03-08 16:07:49 +0100

    checkpoint

--- app/src/main/java/com/example/restaurant/activity/PhotoEditorActivity.java
+++ app/src/main/java/com/example/restaurant/activity/PhotoEditorActivity.java
@@ -1,7 +1,14 @@
 package com.example.restaurant.activity;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.res.ResourcesCompat;
 
@@ -9,32 +16,42 @@ import com.example.restaurant.R;
 import com.example.restaurant.services.RestaurantServices;
 import com.example.restaurant.services.RestaurantServicesImpl;
 
+import ja.burhanrashid52.photoeditor.OnPhotoEditorListener;
 import ja.burhanrashid52.photoeditor.PhotoEditor;
 import ja.burhanrashid52.photoeditor.PhotoEditorView;
+import ja.burhanrashid52.photoeditor.ViewType;
 
-public class PhotoEditorActivity extends AppCompatActivity
-{
-
+public class PhotoEditorActivity extends AppCompatActivity implements OnPhotoEditorListener, View.OnClickListener {
+    private static final int PICK_REQUEST = 53   ;
+    PhotoEditorView mPhotoEditorView;
+    PhotoEditor mPhotoEditor;
+    View mTxtCurrentTool;
+    View mRvTools;
+    View mRvFilters;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stickers);
 
-        PhotoEditorView mPhotoEditorView = findViewById(R.id.photoEditorView);
+        initViews();
+
+        mPhotoEditorView = findViewById(R.id.photoEditorView);
         mPhotoEditorView.getSource().setImageResource(R.drawable.restaurant_placeholder);
 
         Typeface mTextRobotoTf = ResourcesCompat.getFont(this, R.font.roboto_medium);
         //Typeface mEmojiTypeFace = Typeface.createFromAsset(getAssets(), "font/emojione-android.ttf");
 
-        Object mPhotoEditor = new PhotoEditor.Builder(this, mPhotoEditorView)
+        mPhotoEditor = new PhotoEditor.Builder(this, mPhotoEditorView)
                 .setPinchTextScalable(true)
                 .setClipSourceImage(true)
                 .setDefaultTextTypeface(mTextRobotoTf)
                 //.setDefaultEmojiTypeface(mEmojiTypeFace)
                 .build();
 
-        
+        mPhotoEditor.setOnPhotoEditorListener(this);
+
+
 
        /* mPhotoEditor.saveAsFile(filePath, new PhotoEditor.OnSaveListener() {
             @Override
@@ -47,5 +64,92 @@ public class PhotoEditorActivity extends AppCompatActivity
                 Log.e("PhotoEditor","Failed to save Image");
             }
         }); */
+    }
+
+    @Override
+    public void onAddViewListener(@Nullable ViewType viewType, int i) {
+    }
+
+    @Override
+    public void onEditTextChangeListener(@Nullable View view, @Nullable String s, int i) {
+
+    }
+
+    @Override
+    public void onRemoveViewListener(@Nullable ViewType viewType, int i) {
+
+    }
+
+    @Override
+    public void onStartViewChangeListener(@Nullable ViewType viewType) {
+
+    }
+
+    @Override
+    public void onStopViewChangeListener(@Nullable ViewType viewType) {
+
+    }
+
+    @Override
+    public void onTouchSourceImage(@Nullable MotionEvent motionEvent) {
+
+    }
+
+
+    private void initViews() {
+        mPhotoEditorView = findViewById(R.id.photoEditorView);
+        mTxtCurrentTool = findViewById(R.id.txtCurrentTool);
+        mRvTools = findViewById(R.id.rvConstraintTools);
+        mRvFilters = findViewById(R.id.rvFilterView);
+        //mRootView = findViewById(R.id.rootView);
+
+        ImageView imgUndo = findViewById(R.id.imgUndo);
+        imgUndo.setOnClickListener(this);
+
+        ImageView imgRedo = findViewById(R.id.imgRedo);
+        imgRedo.setOnClickListener(this);
+
+        ImageView imgCamera = findViewById(R.id.imgCamera);
+        imgCamera.setOnClickListener(this);
+
+        ImageView imgGallery = findViewById(R.id.imgGallery);
+        imgGallery.setOnClickListener(this);
+
+        ImageView imgSave = findViewById(R.id.imgSave);
+        imgSave.setOnClickListener(this);
+
+        ImageView imgClose = findViewById(R.id.imgClose);
+        imgClose.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.imgUndo){
+                mPhotoEditor.undo();}
+        else if (v.getId() == R.id.imgRedo) {
+            mPhotoEditor.redo();
+        }
+        else if(v.getId() == R.id.imgGallery) {
+            Intent galleryIntent = new Intent();
+            galleryIntent.setType("image/*");
+            galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+            startActivityForResult(Intent.createChooser(galleryIntent, "Select Picture"), PICK_REQUEST);
+        }
+
+
+            //case R.id.imgSave:
+              //  saveImage();
+               // break;
+            //case R.id.imgClose:
+              //  onBackPressed();
+                //break;
+            //case R.id.imgShare:
+              //  shareImage();
+                //break;
+            //case R.id.imgCamera:
+              //  Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                //startActivityForResult(cameraIntent, CAMERA_REQUEST);
+                //break;
+
     }
 }
